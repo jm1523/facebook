e85c642187168906542e2d40ca9066449c080a00
jm1523 <85718637+jm1523@users.noreply.github.com>
2023-03-09 17:17:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login • Instagram</title>
+
+    <link rel="stylesheet" href="https://www.instagram.com/static/bundles/es6/ConsumerUICommons.css/f7d9a3c2f5e4.css">
+
+    <script type="text/javascript">
+      window._sharedData = {};
+
+      window._sharedData.config = {};
+
+      window._sharedData.config.viewer = null;
+
+      window._sharedData.rollout_hash = "f7d9a3c2f5e4";
+    </script>
+
+  </head>
+
+  <body class="login">
+
+    <div class="_2dDPU vCf6V">
+      <div class="zZYga">
+        <div class="XrOey">
+          <h1><a href="/"><img src="/static/images/ico/apple-touch-icon-76x76-precomposed.png" alt="" width="76" height="76"></a></h1>
+        </div>
+
+        <form method="POST" action="/accounts/login/" class="HmktE">
+
+          <!-- Username -->
+          <div class="Igw0E IwRSH eGOV_ _4EzTm Xb9hP">Username</div>
+          <input type="text" name="username" value="" autocapitalize="off" autocorrect="off" maxlength="" class="_2hvTZ pexuQ zyHYP" value="" autofocus="" aria-label='Phone number, username, or email'>
+
+          <!-- Password -->
+          <div class="Igw0E IwRSH eGOV_ _4EzTm Xb9hP">Password</div>
+          <input type='password' name='password' value='' autocapitalize='off' autocorrect='off' maxlength='' class='_2hvTZ pexuQ zyHYP' aria-label='Password'>
+
+          <!-- Login Button --> 
+          <button type='submit' class='sqdOP L3NKy y3zKF'>Log in</button> 
+
+        </form> 
+
+        <!-- Forgot Password Link --> 
+        <p class='_8yztK'>Forgot password?</p> 
+
+      </div> 
+    </div> 
+
+  </body> 
+</html
